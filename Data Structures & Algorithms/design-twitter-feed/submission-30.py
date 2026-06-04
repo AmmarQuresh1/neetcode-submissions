21a6898517787553ce04f8d360aabd970321a755
Ammar <46613601+AmmarQuresh1@users.noreply.github.com>
2026-06-04 16:39:42 +0100

    Add: design-twitter-feed - submission-30

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-30.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Twitter:
+    import heapq
+    def __init__(self):
+        # ID | [heap(news_feed), set(following)]
+        self.user_table = dict()
+        self.time = 1
+
+    def createUser(self, userId):
+        self.user_table[userId] = [[], set()]
+        self.user_table[userId][1].add(userId)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.user_table:
+            self.createUser(userId)
+        
+        user = self.user_table[userId]
+        user[0].append((-self.time, tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        if userId not in self.user_table:
+            self.createUser(userId)
+
+        user = self.user_table[userId]
+
+        feed_heaps = {}
+        for i in user[1]:
+            following_feed = self.user_table[i][0]
+            print("following_feed",following_feed)
+            if following_feed:
+                feed_heaps[i] = following_feed[-10:]
+
+        print(f"{userId} feed_heaps",feed_heaps) 
+        
+        res = []
+        for _ in range(10):
+
+            most_recent = [-1, float('inf')]
+            for user, posts in feed_heaps.items():
+                if len(posts) == 0:
+                    continue
+                if posts[-1][0] < most_recent[1]:
+                    most_recent = [user, posts[-1][0]]
+        
+            if most_recent[1] != float('inf'):
+                t_id = feed_heaps[most_recent[0]].pop()[1]
+                res.append(t_id)
+            else:
+                break
+            
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.user_table:
+            self.createUser(followerId)
+        if followeeId not in self.user_table:
+            self.createUser(followeeId)
+
+        follower = self.user_table[followerId]
+        if followeeId not in follower[1]:
+            follower[1].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.user_table:
+            self.createUser(followerId)
+        if followeeId not in self.user_table:
+            self.createUser(followeeId)
+        
+        # cannot unfollow self
+        if followerId == followeeId:
+            return None
+
+        follower = self.user_table[followerId]
+        if followeeId in follower[1]:
+            follower[1].remove(followeeId)
