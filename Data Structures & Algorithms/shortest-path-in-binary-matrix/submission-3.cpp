6ab91bca67d035c14dfe07af54888e8515664e7e
Ammar <46613601+AmmarQuresh1@users.noreply.github.com>
2026-06-19 14:51:31 +0100

    Add: shortest-path-in-binary-matrix - submission-3

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-3.cpp
@@ -0,0 +1,46 @@
+auto speedup = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    struct Cell {
+        int i;
+        int j;
+        int dist;
+    };
+
+    struct Point {
+        int i;
+        int j;
+    };
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        deque<Cell> q;
+
+        if (grid[0][0] == 1) return -1;
+
+        q.push_back({0, 0, 1});
+        grid[0][0] = 1;        
+        while (!q.empty()) {
+            auto [i, j, dist] = q.front();
+            q.pop_front();
+
+            if (i == static_cast<int>(grid.size())-1 && j == static_cast<int>(grid[i].size())-1)
+                return dist;
+            
+            vector<Point> directions {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+            for (auto& [x,y] : directions){
+                if ((i+x >= static_cast<int>(grid.size()) || j+y >= static_cast<int>(grid[i].size())
+                    || i+x < 0 || j+y < 0) || (grid[i+x][j+y] == 1))
+                    continue;
+                q.push_back({i+x, j+y, dist + 1});
+                grid[i+x][j+y] = 1;
+            }
+        }
+
+        return -1;
+    }
+};
