b0fbd5e7d445d86a18112e4c216f1528d058e218
Ammar <46613601+AmmarQuresh1@users.noreply.github.com>
2026-06-22 16:54:19 +0100

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& seen) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int size{};
+
+        if (i < 0 || j < 0 || i >= rows || j >= cols) {
+            return size;
+        }
+        if (grid[i][j] == 0) {
+            return size;
+        }
+        if (seen[i][j] == true) {
+            return size;
+        }
+        seen[i][j] = true;
+        ++size;
+
+        size += dfs(i+1, j, grid, seen);
+        size += dfs(i-1, j, grid, seen);
+        size += dfs(i, j+1, grid, seen);
+        size += dfs(i, j-1, grid, seen);
+        
+        return size;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        std::vector<std::vector<bool>> seen(rows, std::vector<bool>(cols, false));
+
+        int max_size{};
+        for (int i{}; i != rows; ++i) {
+            for (int j{}; j != cols; ++j) {
+                max_size = std::max(dfs(i, j, grid, seen), max_size);
+                std::cout<<i<<j<<" "<<max_size<<'\n';
+            }
+        }
+
+        return max_size;
+    }
+};
