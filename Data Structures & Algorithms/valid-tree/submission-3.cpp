ddf9d90ce88bd9df4b6afb8d3bdb0380f71586b7
Ammar <46613601+AmmarQuresh1@users.noreply.github.com>
2026-06-26 14:28:14 +0100

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (size(edges) != n-1) return false;
+
+        std::array<int, 100> parent{};
+        for (int i = 0; i < 100; ++i) {
+            parent[i] = i;
+        }
+
+        for (auto vi : edges) {
+            int temp = vi[0];
+            while (temp != parent[temp]) {
+                temp = parent[temp];
+            }
+            int temp2 = vi[1];
+            while (temp2 != parent[temp2]) {
+                temp2 = parent[temp2];
+            }
+            if (temp == temp2) return false;
+
+            parent[temp] = temp2;
+        }
+
+        return true;
+    }
+};
