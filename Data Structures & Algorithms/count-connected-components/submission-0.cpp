5f33dfc27543b94295c44d51c8675ab9763c6287
Ammar <46613601+AmmarQuresh1@users.noreply.github.com>
2026-06-29 11:55:18 +0100

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        std::unordered_set<int> seen_nodes{};
+        int count = 0;
+        for (vector<int>& vi : edges) {
+            if (seen_nodes.find(vi[0]) == seen_nodes.end()) ++count;
+            seen_nodes.insert(vi[0]);
+            seen_nodes.insert(vi[1]);
+        }
+
+        return count;
+    }
+};
